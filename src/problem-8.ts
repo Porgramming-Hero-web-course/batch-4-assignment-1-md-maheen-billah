fcee1e0754348a90f8d2008353b8064903275e38
md-maheen-billah <md.maheen.billah@icloud.com>
2024-11-07 23:42:03 +0100

    problem 8

---
+++ src/problem-8.ts
@@ -0,0 +1,14 @@
+{
+    //
+
+    function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+        for (let key of keys) {
+            if (!(key in obj)) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    //
+}
